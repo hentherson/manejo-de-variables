4710569cb18685d4085bf43c2a543631d65e3757
= <jjrb6@hotmail.com>
2019-02-09 18:02:30 -0500

    Enseñanza de variables y impresion

---
+++ impresion.php
@@ -0,0 +1,12 @@
+<?php
+
+$numero = 12345;
+$array = ['nombre' => 'Jeremy'];
+
+echo $numero;
+echo "<br>";
+print $numero;
+echo "<br>";
+print_r($array);
+echo "<br>";
+var_dump($array);
---
+++ variables.php
@@ -0,0 +1,10 @@
+<?php
+
+$numero = 1234;
+$cadena_1 = 'Cadena 1';
+$cadena_2 = "Cadena 2";
+$flotante = 10.9;
+$booleanos = true;
+$arreglos_1 = [];
+$arreglos_2 = array();
+
