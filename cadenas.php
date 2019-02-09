87c67ac061619c29e7acc57cae20917b5d020377
= <jjrb6@hotmail.com>
2019-02-09 18:13:29 -0500

    Explicación de cadenas, contatenaciones y interpolación

--- cadenas.php
+++ cadenas.php
@@ -1,1 +1,19 @@
 <?php
+
+$nombre = 'Jeremy';
+$apelido = "Reyes";
+$edad = '28';
+
+echo $nombre . ' ' . $apelido;  // Impresion normal
+echo '<br>';
+echo '$nombre';                 // Interpolación de cadenas con comillas simples no se puede.
+echo '<br>';
+echo "$nombre";                 // Interpolación de cadenas con comillas dobles si funciona.
+echo '<br>';
+echo "{$nombre}";               // Interpolación con buenas practicas.
+echo '<br>';
+echo $nombre . ' ' . $apelido . ' con edad de '. $edad;
+echo '<br>';
+echo "{$nombre} {$apelido} con edad de {$edad}";
+echo '<br>';
+echo "$nombre $apelido con edad de $edad";
